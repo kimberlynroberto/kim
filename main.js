a76051e0c64eda067796742ce9aecbec054b807f
kimberlynroberto <144706907+kimberlynroberto@users.noreply.github.com>
2023-09-20 12:27:06 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function tocaSomPom(){
+document.querySelector('#som_tecla_pom').play();
+document.querySelector('.tecla_pom').onclick = tocaSomPom;}
+
+function tocaSomClap(){
+    document.querySelector('#som_tecla_Clap').play();
+    document.querySelector('.tecla_Clap').onclick = tocaSomClap;}
+
+ 
+    function tocaSomTim(){
+        document.querySelector('#som_tecla_Tim').play();
+        document.querySelector('.tecla_Tim').onclick = tocaSomTim;}
+
+        function tocaSomPuff(){
+            document.querySelector('#som_tecla_Puff').play();
+            document.querySelector('.tecla_Puff').onclick = tocaSomPuff;}
+
+            function tocaSomSplash(){
+                document.querySelector('#som_tecla_Splash').play();
+                document.querySelector('.tecla_Splash').onclick = tocaSomSplash;}
+
+        
